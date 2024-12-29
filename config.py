068001d1627c47606f62b143fbccf5ed136c3b19
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-12-29 16:26:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,21 @@
 import os
 
-#Bot token @Botfather
+# Bot token @Botfather
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-#Your API ID from my.telegram.org
+# Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "21128733"))
 
-#Your API Hash from my.telegram.org
+# Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "13f1c613ae0775aa68f05cd3df5531c7")
 
-#Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority")
+# Your Owner / Admin Id For Broadcast 
+ADMINS = int(os.environ.get("ADMINS", "6231550362"))
+
+# Your Mongodb Database Url
+# Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = os.environ.get("DB_NAME", "EXONTESTMONGO")
+
+# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
+ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
