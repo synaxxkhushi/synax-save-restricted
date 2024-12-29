d957e0ecb0a82bc6a75582abd3c0330f91822a68
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-12-29 16:36:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,6 @@ API_ID = int(os.environ.get("API_ID", "21128733"))
 # Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "13f1c613ae0775aa68f05cd3df5531c7")
 
-# Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "6231550362"))
-
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
